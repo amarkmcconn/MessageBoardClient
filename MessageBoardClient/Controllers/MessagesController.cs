6003cc65885e88996bb351c8544e8ad4ab5ea4ce
JeremyM45 <jeremyvs1945@gmail.com>
2022-06-08 14:00:53 -0700

    add view/post views/controllers
    Co-authored-by: Mark McConnell <mark.programming1@gmail.com>

---
+++ MessageBoardClient/Controllers/MessagesController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using MessageBoardClient.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using System.Collections.Generic;
+
+namespace MessageBoardClient.Controllers
+{
+  public class MessagesController : Controller
+  {
+    public IActionResult Index()
+    {
+      var allMessages = Message.GetMessages();
+      return View(allMessages);
+    }
+    public IActionResult Details(int id)
+    {
+      var message = Message.GetDetails(id);
+      return View(message);
+    }
+    public ActionResult Create()
+    {
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Create(Message message)
+    {
+      message.Date = System.DateTime.Now;
+      Message.Post(message);
+      return RedirectToAction("Index");
+    }
+  }
+}
